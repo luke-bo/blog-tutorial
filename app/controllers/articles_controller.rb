81ed316754ad9fa33134bb74cb10651b0bc73535
luke.bohonis <w.bohonis@gmail.com>
2019-10-21 16:38:23 -0700

    add crud functionality for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,51 @@
 class ArticlesController < ApplicationController
-    def new
-    
+    def index
+      @articles = Article.all
     end
+   
+    def show
+      @article = Article.find(params[:id])
+    end
+   
+    def new
+      @article = Article.new
+    # The reason why we added @article = Article.new in the ArticlesController is that otherwise @article would be nil in our view, and calling @article.errors.any? would throw an error.
+
+    end
+   
+    def edit
+      @article = Article.find(params[:id])
+    end
+   
+    def create
+      @article = Article.new(article_params)
+   
+      if @article.save
+        redirect_to @article
+      else
+        render 'new'
+      end
+    end
+   
+    def update
+      @article = Article.find(params[:id])
+   
+      if @article.update(article_params)
+        redirect_to @article
+      else
+        render 'edit'
+      end
+    end
+   
+    def destroy
+      @article = Article.find(params[:id])
+      @article.destroy
+   
+      redirect_to articles_path
+    end
+   
+    private
+      def article_params
+        params.require(:article).permit(:title, :text)
+      end
 end
